122b7ea6c0a4202ded18f008b48ca69cd5490986
Denis Rakitin <piggest@mail.ru>
2019-11-09 01:23:58 +0100

    Initial Commit

---
+++ SlidePuzzleGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlidePuzzleGame
+//
+//  Created by Denis Rakitin on 2019-11-09.
+//  Copyright © 2019 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
